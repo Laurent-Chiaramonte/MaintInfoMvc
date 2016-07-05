e02697ed05e2bfed1fed0defc0a1cb75f1720c57
Laurent-Chiaramonte <chiaramonte.laurent@gmail.com>
2016-07-05 11:48:24 +0200

    InitClient

---
+++ MaintInfoMvc/MaintInfoMvc/Controllers/ClientController.cs
@@ -0,0 +1,27 @@
+﻿using MaintInfoMvc.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MaintInfoMvc.Controllers
+{
+    public class ClientController : Controller
+    {
+        private IDal dal;
+        
+        public ClientController() : this(new Dal()) { }
+
+        public ClientController(IDal dalIoc)
+        {
+            this.dal = dalIoc;
+        }
+
+        public ActionResult Index()
+        {
+            List<Client> listeDesClients = dal.ObtientTousLesClients();
+            return View(listeDesClients);
+        }
+    }
+}
---
+++ MaintInfoMvc/MaintInfoMvc/Models/Init.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace MaintInfoMvc.Models
+{
+    public class Init : DropCreateDatabaseAlways<BddContext>
+    {
+        protected override void Seed(BddContext context)
+        {
+            // Clients
+            context.Clients.Add(new Client
+            {
+                num_client = 1,
+                nom_client = "Panzani",
+                adresse_client = "34, avenue de corot",
+                cp_client = "13013",
+                ville_client = "Marseille",
+                tel_client = "0491609023"
+            });
+            context.Clients.Add(new Client
+            {
+                num_client = 2,
+                nom_client = "Heineken",
+                adresse_client = "14, rue François Chardigny",
+                cp_client = "13011",
+                ville_client = "Marseille",
+                tel_client = "0491691322"
+            });
+
+            // Centres
+            context.Centres.Add(new CentreInformatique
+            {
+                 num_centre = 1,
+                 adresse_centre = "34, avenue de corot",
+                 cpl_centre = "13013",
+                 ville_centre = "Marseille",
+                 tel_centre = "0491609023",
+                 //Client = 1
+            });
+
+            // Contrats
+        }
+    }
+}
